29272086461c9c982797c80c56f661b6e9ce3b28
william <william@bowei.page>
2020-06-22 13:20:02 +0800

    手動佈署測試

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,20 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        william_nuxt
-      </h1>
+      <h1 class="title">william_nuxt</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
           class="button--green"
-        >
-          Documentation
-        </a>
+        >手動佈署測試</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
-        >
-          GitHub
-        </a>
+        >GitHub</a>
       </div>
     </div>
   </div>
@@ -48,16 +42,8 @@ export default {
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
